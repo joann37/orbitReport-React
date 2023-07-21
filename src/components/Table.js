7bfa1c94ba7d2ae477fa0be3791b29045d25e516
JoJo <127879448+joann37@users.noreply.github.com>
2023-07-21 12:36:56 -0500

    added completed coomponents button table and app

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,17 +1,26 @@
 const Table = ({ sat }) => {
   return (
-      <table>
-       <thead>
+    <table>
+    <thead>
+     <tr>
+     <th>Name</th>
+     <th>Type of Satellite</th>
+     <th>Launch Date</th>
+     <th>Status</th>
+     </tr>
+     </thead>
+     <tbody>
+       {sat.map((data, id)=> { 
         <tr>
-          <th>Header TBD</th>
+          <td>{data.name}</td>
+          <td>{data.typeOfSatellite}</td>
+          <td>{data.launchDate}</td>
+          <td>{data.status}</td>
         </tr>
-        </thead>
-        <tbody>
-        <tr>
-          <td>Row Data TBD</td>
-        </tr>
-        </tbody>
-      </table>
+       })}
+       
+     </tbody>
+   </table>
   );
 };
 
