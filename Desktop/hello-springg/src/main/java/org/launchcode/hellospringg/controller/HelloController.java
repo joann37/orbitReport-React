8a7fb3eb4c65b632cf082cdc16184119717f1e5f
JoJo <127879448+joann37@users.noreply.github.com>
2023-10-20 10:45:53 -0500

    controllerone

--- Desktop/hello-springg/src/main/java/org/launchcode/hellospringg/controller/HelloController.java
+++ Desktop/hello-springg/src/main/java/org/launchcode/hellospringg/controller/HelloController.java
@@ -7,13 +7,13 @@ import org.springframework.web.bind.annotation.*;
 @ResponseBody
 @RequestMapping("hello")
 public class HelloController {
-//    //Handles request at path /hello
-////    @GetMapping("hello")
-////    @ResponseBody
-////    public String hello() {
-////        return "Hello, Spring! What is Popping?";
+//      Handles request at path /hello
+//      @GetMapping("hello")
+//      @ResponseBody
+//      public String hello() {
+//       return "Hello, Spring! What is Popping?";
 //    }
-    //lives /hello/seeYa
+    //lives at /hello/seeYa
     @GetMapping("seeYa")
     public String seeYa() {
         return "SeeYa Later, Spring! Hope all is well!";
@@ -26,19 +26,21 @@ public class HelloController {
     public String helloWithQueryParam(@RequestParam String name){
         return "Hi there "+ name + "!!";
     }
+
     //Handles request of the form hello/launchCode
-    @GetMapping("hello/{name}")
+    @GetMapping("{name}")
     public String helloWithPathParam(@PathVariable String name){
         return "Hi there now my good buddy "+ name + "!!";
     }
 
+    //lives at /hello/form
     @GetMapping("form")
     public String helloForm(){
         return "<html>" +
                 "<body>" +
                 "<form action='hello' method='post'>" + // submit a request to /hello
                 "<input type='text' name='name'>" +
-                "<input type='submit' value='Greet me now!'>" +
+                "<input type='submit' value='Greet me Regal!!'>" +
                 "</form>" +
                 "</body>" +
                 "</html>";
